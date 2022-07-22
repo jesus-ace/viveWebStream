df46e4b4d147f70c71bb8a18869b0b82c718b1e3
Jesús Acevedo <jasve504@gmail.com>
2022-07-21 20:55:52 -0400

    se crear pantalla par desplegar la series

--- Modules/Page/Resources/views/section-series/serie-index.blade.php
+++ Modules/Page/Resources/views/section-series/serie-index.blade.php
@@ -1,7 +1,130 @@
 @extends('page::layouts.master')
 @section('content')
 @include('page::navbar')
-<div class="main-content">
+
+<style>
+     .serie-content{
+        position: relative;
+        min-width: 100%;
+        z-index: -1000;
+    }
+    .serie-content::before{
+        position: absolute;
+        content: '';
+        width: 100%;
+        height: 100%;
+        /* background: linear-gradient(rgba(5, 68, 141, 0.486), #030303); */
+        background: linear-gradient(to bottom,rgba(8,15,40,0) 0%,rgba(8,15,40,1) 100%);
+        top: 0;
+        left: 0;
+        z-index: -1000;
+    }
+    .addlist > .Button {
+        padding: .5rem .7rem;
+        width: 100%;
+        border-radius: .3rem;
+        text-align: left;
+        background-color: #1e2747;
+        font-size: .75rem;
+        border: none;
+        color: #fff;
+
+        border: 0;
+        cursor: pointer;
+        padding: .5rem 2rem;
+        display: inline-block;
+        vertical-align: top;
+        text-align: center;
+        line-height: 2rem;
+
+        font-size: 1rem;
+        padding: .5rem 1rem
+    }
+
+    .addlist > .Button > i {
+        color: #edb709;
+        float: left;
+        margin-right: .5rem;
+    }
+    .addlist > .Button .ddw {
+        float: right;
+        color: #007aff;
+        float: right;
+        margin-right: 0;
+        font-size: .65rem;
+    }
+    .tvshow {
+        position: relative;
+        margin-top: 1rem;
+        padding-left: 67px;
+        padding-top: 12px;
+    }
+    .tvshow::before {
+        width: 56px;
+        height: 60px;
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: url(/image/tv.png) no-repeat;
+    }
+    .select-season {
+        margin-bottom: 2.5rem;
+    }
+    .select-season > span {
+        float: left;
+        padding: .5rem 1.5rem .5rem 1.25rem;
+        border-radius: 3px;
+        background-color: #141a32;
+        color: #4f6b95;
+        margin-right: -.5rem;
+        line-height: calc(2rem - 2px);
+    }
+    .select-season > select {
+        float: left;
+        width: auto;
+        border-radius: 3px;
+        background-color: #1e2747;
+        border: 0;
+    }
+    input, textarea, select {
+        border: 1px solid #1e2747;
+        background-color: #1e2747;
+        color: #8da0bc;
+    }
+    select {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPScjMDA3QUZGJyB2aWV3Qm94PScwIDAgMjQgMjQnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZD0nTTcuNDEgNy44NEwxMiAxMi40Mmw0LjU5LTQuNThMMTggOS4yNWwtNiA2LTYtNnonLz48cGF0aCBkPSdNMC0uNzVoMjR2MjRIMHonIGZpbGw9J25vbmUnLz48L3N2Zz4=);
+        background-position: right 1rem center;
+        background-repeat: no-repeat;
+        background-size: 1.5rem;
+        padding-right: 3rem;
+    }
+    input, textarea, select {
+        display: block;
+        padding: .5rem 1.5rem;
+        padding-right: 1.5rem;
+        width: 100%;
+        line-height: calc(2rem - 2px);
+        border-radius: 3rem;
+    }
+    button, input, select, textarea {
+        font: inherit;
+            line-height: inherit;
+        width: 100%;
+        vertical-align: middle;
+        line-height: normal;
+        text-transform: none;
+        overflow: visible;
+        color: inherit;
+        transition: .2s;
+    }
+</style>
+
+<div class="main-content serie-content" style="background-image: url({{$series[0]->cover}}); background-repeat: no-repeat;
+    background-size: cover">
     @include('page::section-series.serie')
 </div>
 
--- Modules/Page/Resources/views/section-series/serie.blade.php
+++ Modules/Page/Resources/views/section-series/serie.blade.php
@@ -3,156 +3,48 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-12 overflow-hidden">
-        <div class="iq-main-header d-flex align-items-center justify-content-between">
-          {{-- <h4 class="main-title">Trending</h4> --}}
-        </div>
-        <div class="trending-contens">
-          <ul id="trending-slider-nav" class="list-inline p-0 mb-0 row align-items-center">
-            <li>
-              <a href="javascript:void(0);">
-                <div class="movie-slick position-relative">
-                  <img src="{{$serie->cover}}" class="img-fluid" alt="">
+        {{-- <div class="iq-main-header d-flex align-items-center justify-content-between">
+          <h4 class="main-title">Trending</h4>
+        </div> --}}
+        <div class="serie-head">
+          <div class="row">
+              <div class="col-3">
+                <figure style="margin-bottom: 1rem">
+                  <img src="{{$series[0]->cover}}" alt="" width="285" height="278" style="object-position: 50% 50% !important;">
+                </figure>
+                <div class="addlist"> 
+                  <button type="button" class="Button  aa-mdl" data-mdl="mdl-login"><i class="fa-plus-square"></i>Agregar a... <i class="fa-chevron-down ddw"></i></button>
                 </div>
-              </a>
-            </li>
-          </ul>
-          <ul id="trending-slider" class="list-inline p-0 m-0 d-flex align-items-center">
-            <li>
-              <div class="tranding-block position-relative" style="background-image: url({{$serie->cover}});">
-                <div class="trending-custom-tab">
-                  <div class="tab-title-info position-relative">
-                    <ul class="trending-pills d-flex nav nav-pills justify-content-center align-items-center text-center" role="tablist">
-                      <li class="nav-item">
-                        <a href="#trending-data1" class="nav-link active show" data-toggle="pill" role="tab" aria-selected="true">
-                          Sinopsis
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="#trending-data2" class="nav-link" data-toggle="pill" role="tab" aria-selected="false">
-                          Episodios
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="trending-content">
-                    <div id="trending-data1" class="overview-tab tab-pane fade active show">
-                      <div class="trending-info align-items-center w-100 animated fadeInUp">
-                        <a href="javascript:void(0);" tabindex="0">
-                          <div class="res-logo">
-                            <div class="channel-logo">
-                              {{-- <img src="images/logo.png" class="c-logo" alt=""> --}}
-                              <h1>{{$serie->name}}</h1>
-                            </div>
-                          </div>
-                        </a>
-                        {{-- <h1 class="trending-text big-title text-uppercase">The Crown</h1> --}}
-                        <div class="d-flex align-items-center text-white text-detail mt-3">
-                          <span class="badge badge-secondary p-3">{{$serie->categorias->name}}</span>
-                          {{-- <span class="ml-3">1 Temporada</span> --}}
-                          <span class="trending-year">2022</span>
-                        </div>
-                        <div class="d-flex align-items-center series mb-4">
-                          <a href="javascript:void(0);">
-                            <img src="" class="img-fluid" alt="">
-                          </a>
-                          <span class="text-gold ml-3">Producido : Vive TV</span>
-                        </div>
-                        <p class="trending-dec">
-                          {{$serie->sinopsis}}
-                        </p>
-                        <div class="p-btns">
-                          <div class="d-flex align-items-center p-0">
-                            <a href="#" class="btn btn-hover mr-2" tabindex="0">
-                              <i class="fa fa-play mr-2"></i>
-                              Ver ahora
-                            </a>
-                            {{-- <a href="#" class="btn btn-link" tabindex="0">
-                              <i class="fa fa-plus pr-2"></i>
-                              My List 
-                            </a> --}}
-                          </div>
-                        </div>
-                        <div class="trending-list mt-4">
-                          <div class="text-primary title">
-                            Productores : 
-                            <span class="text-body">
-                              Vive tv, Vive tv, Vive tv
-                            </span>
-                          </div>
-                          {{-- <div class="text-primary title">
-                            Genres :
-                            <span class="text-body">
-                              Biography
-                            </span>
-                          </div>
-                          <div class="text-primary title">
-                            Creators :
-                            <span class="text-body">
-                              Peter Morgan
-                            </span>
-                          </div> --}}
-                        </div>
-                      </div>
-                    </div>
-
-                    <div id="trending-data2" class="overlay-tab tab-pane fade">
-                      <div class="trending-info align-items-center w-100 animated fadeInUp">
-                        <a href="#" tabindex="0">
-                          <div class="channel-logo">
-                              <h1>Cine Corto</h1>
-                          </div>
-                        </a>
-                        {{-- <h1 class="trending-text big-title text-uppercase">the Crown</h1> --}}
-                        <div class="d-flex align-items-center text-white text-detail mb-4 mt-2">
-                          <span class="season_date">
-                           Temporada 1
-                          </span>
-                          <span class="trending-year">Ago 2022</span>
-                        </div>
-                        <div class="iq-custom-select d-inline-block sea-epi">
-                          <select name="Seasons" class="form-control season-select" id="sessons">
-                            @foreach ($sessons as $sesson)
-                              <option value="{{$sesson->temporada_id}}">Temporada {{$sesson->temporada_id}}</option> 
-                            @endforeach
-                          </select>
-                        </div>
-
-                        <div class="episodes-contens mt-4">
-                          <div class="owl-carousel owl-theme episodes-slider1 list-inline p-0 m-0">
-                            @foreach ($series as $seriescap)
-                                @foreach ($seriescap->capitulo as $e_item)
-                                  <div class="e-item">
-                                    <div class="block-image position-relative">
-                                      <a href="#">
-                                        <img src="{{$e_item->cover}}" class="img-fluid" alt="">
-                                      </a>
-                                      <div class="episode-play-info">
-                                        <div class="episode-play" data-toggle="modal" data-target="#modal{{$e_item->id}}">
-                                          <a href="#" tabindex="0">
-                                            <i class="fa fa-play"></i>
-                                          </a>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div class="episodes-description text-body">
-                                      <div class="d-flex align-items-center justify-content-between">
-                                        <a href="#" data-toggle="modal" data-target="#modal{{$e_item->id}}">{{$e_item->name}}</a>
-                                      </div>
-                                      <p class="mb-0">{{$e_item->sub_name}}</p>
-                                    </div>
-                                  </div>
-                                @endforeach
-                                
-                            @endforeach
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                <div class="tvshow">
+                  <p><span></span> Temporadas</p><p><span></span> Capitulos</p>
+                  <div class="cv3-dots" aria-hidden="true"> <span class="cv3-quality" title="En emision"><i></i></span></div>
                 </div>
               </div>
-            </li>
-          </ul>
+              <div class="col-9 ">
+                <header>
+                  <h1 style="font-size: 2.25rem; line-height: 1.5; color: #fff; font-weight: 700; margin-bottom: 0;">{{$series[0]->name}}</h1>
+                  <h2 style="font-size: 1rem; margin-bottom: .5rem; line-height: 1.5rem; font-weight: 400; ">{{$series[0]->name}}</h2>
+                </header>
+                <div style="max-height: 100px; overflow: auto; margin-bottom: 1rem; color: #8da0bc, clear: none;">
+                  <p>{{$series[0]->sinopsis}}</p>
+                </div>
+                <div class="MvTbCn on anmt" id="MvTb-Info" style="overflow: hidden;">
+                  <ul class="InfoList" style="font-size: .875rem;"> 
+                    <li class="AAIco-adjust" style="list-style-type: none; color: #8da0bc"><strong>Director:</strong> <span class="" style="color: #fff">Nathan Fielder</span></li>
+                    <li class="AAIco-adjust" style="list-style-type: none; color: #8da0bc"><strong>Genero:</strong> <a href="">Comedy</a> </li>
+                    <li class="AAIco-adjust loadactor" style="list-style-type: none; color: #8da0bc"><strong>Actores:</strong> <a class="tt-at" href="">Nathan Fielder</a> 
+                    </li>
+                  </ul>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="select-season"> 
+          <span>Seleccionar temporada</span> 
+          <select name="" id="select-season">
+            <option value="1">Temporada 1</option>
+            <option value="2">Temporada 2</option> 
+          </select>
         </div>
       </div>
     </div>
